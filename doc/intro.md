ce026d2606e35057d87cf86e2e63375467aabebe
Matthew Carter <m@ahungry.com>
2019-09-06 01:09:23 -0400

    all

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to easy-types
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/easy_types/core_test.clj
@@ -0,0 +1,7 @@
+(ns easy-types.core-test
+  (:require [clojure.test :refer :all]
+            [easy-types.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
