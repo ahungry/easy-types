2d1461303f7f33bee9fedb5c247f7093c7f1a4a1
Matthew Carter <m@ahungry.com>
2019-09-06 01:16:01 -0400

    works with this so far...

--- src/easy_types/core.clj
+++ src/easy_types/core.clj
@@ -1,5 +1,25 @@
 (ns easy-types.core
+  (:require
+   [clojure.repl :refer :all]
+   [clojure.test :as ct]
+   [clojure.spec.alpha :as s]
+   [clojure.spec.gen.alpha :as sg]
+   [clojure.spec.test.alpha :as st]
+   )
   (:gen-class))
+
+;; Spec out a 0 arity function
+(s/fdef get-42 :args (s/cat) :ret int? :fn #(= 42 (:ret %)))
+(defn get-42 [] 42)
+(st/instrument `get-42)
+(st/check `get-42)
+
+;; Spec out a 0 arity function that will fail the spec
+(s/fdef yget-42 :args (s/cat) :ret int? ;; :fn #(= 42 (:ret %))
+        )
+(defn yget-42 [] "42")
+(st/instrument `yget-42)
+(st/check `yget-42)
 
 (defn -main
   "I don't do a whole lot ... yet."
